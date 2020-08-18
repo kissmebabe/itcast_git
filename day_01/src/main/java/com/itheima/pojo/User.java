3cf413115b7aaa4c09b57356684f4f58e7eaf241
littlebai <1748455262@qq.com>
2020-08-18 17:48:52 +0800

    firstCommit

---
+++ day_01/src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,31 @@
+package com.itheima.pojo;
+
+/**
+ * com.itheima.pojo
+ *
+ * @ClassName User
+ * @description:
+ * @author: Littlebai
+ * @time: Created in 2020/08/18 17:45
+ * @Version 1.0
+ */
+public class User {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
